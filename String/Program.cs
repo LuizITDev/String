cc6c380cf83deea06d8e2e445f96d3e92e87dd65
LuizLegacies <luiz.job@outlook.com>
2025-02-19 20:35:57 -0300

    Initial commit

---
+++ String/Program.cs
@@ -0,0 +1,46 @@
+﻿
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var paragrafo = "   C# é uma linguaguem moderna e versátil, " +
+                        "Com C# consigo desenvolver para Web, Desktop, Jogos,  " +
+                        "Mobile, entre outros. ";
+
+// Length conta tudo o que tem 
+        var tamanho = paragrafo.Length;
+        Console.WriteLine(tamanho);
+// Empaty vazio
+        var vazio = string.Empty;
+        Console.WriteLine(vazio);
+// ToLower, ToUpper
+        var paragrafoMinusculo = paragrafo.ToLower();
+        Console.WriteLine(paragrafoMinusculo);
+        var paragrafoMaiusculo = paragrafo.ToUpper();
+        Console.WriteLine(paragrafoMaiusculo);
+
+// Split
+        var frases = paragrafo.Split('.');
+        Console.WriteLine(frases);
+
+//Trim, TrimEnd, TrimStart
+        var paragrafoTrim = paragrafo.Trim();
+        Console.WriteLine(paragrafoTrim);
+        var paragrafoTrimend = paragrafo.TrimEnd();
+        Console.WriteLine(paragrafoTrimend);
+        var paragrafoTrimStart = paragrafo.TrimStart();
+        Console.WriteLine(paragrafoTrimStart);
+
+// IsNullOrWhiteSpace
+        var IsNullOrWhiteSpace = string.IsNullOrWhiteSpace(paragrafo);
+        Console.WriteLine(IsNullOrWhiteSpace);
+
+// Replace
+        var ParagrafoCsharp = paragrafo.Replace("C#", "C-sharp");
+        Console.WriteLine(ParagrafoCsharp);
+
+        Console.ReadKey();
+    }
+    
+}
